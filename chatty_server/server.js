22b0f15032af1abc97434e52c3cebe577dc8bc76
zhonghaoliu <joshualiu94@gmail.com>
2017-11-02 17:47:03 -0400

    fix problem: user offline number doesn't change

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -57,9 +57,14 @@ wss.on('connection', (ws) => {
   });
   
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    wss.clients.forEach(function(client) {
+      client.send(JSON.stringify({type: "online", content: `${wss.clients.size} user(s) online`}));
+    });
+  });
 
-
+  
 });
 
 
